ce0ea13ed26e7cd33bb4e66a819e130379f907b2
iyage <yahg.concept@gmail.com>
2022-06-03 10:41:18 -0700

    all commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,14 @@
 #include <stdio.h>
 /* more headers goes there */
 /* main - Entry point */
-/* *
- * betty style doc for function main goes there 
+
+/**
  * main - Entry point
- * */
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 int n;
