af572d0fee2d1da9da648169aea3706de2f7f8b2
iyage <yahg.concept@gmail.com>
2022-06-03 10:17:48 -0700

    all commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(n>0)
     {
-    printf("%d  is positive");
+    printf("%d  is positive", n);
     }
     else if(n<0)
     {
-    printf("%d is negative");
+    printf("%d is negative", n);
 
     }
     else
     {
-    printf("%d is zero");
+    printf("%d is zero", n);
     }
 	return (0);
 }
