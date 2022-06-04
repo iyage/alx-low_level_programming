24abb5c0a944f645ad67a72b53b3e497aa7a1faf
iyage <yahg.concept@gmail.com>
2022-06-04 01:31:25 -0700

    all commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+/* your code goes there */
+
+fprintf(stderr, "error\n");
+return (1);
+}
