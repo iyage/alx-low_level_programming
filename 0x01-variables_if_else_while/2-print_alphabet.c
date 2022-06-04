c51acc9ec59e7b50b828d1e1aaa7223fec68de39
iyage <yahg.concept@gmail.com>
2022-06-03 18:37:55 -0700

    all commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c ;
+
+/* your code goes there */
+for(c = 'a' ; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
