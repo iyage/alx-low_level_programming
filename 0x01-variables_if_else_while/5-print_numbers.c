05c3c651914fa2959d60c793c600e1dad49e3d93
iyage <yahg.concept@gmail.com>
2022-06-03 19:16:59 -0700

    all commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+/* your code goes there */
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
