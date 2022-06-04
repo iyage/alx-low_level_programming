cbd8d6e9ef6c8bb8c9c0ab088b952e4de4b483bd
iyage <yahg.concept@gmail.com>
2022-06-04 01:36:24 -0700

    all commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,6 +13,7 @@ int main(void)
 
 /* your code goes there */
 
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, 
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
