40f98461f137d9f8a95f0f344b06ca85bed07950
iyage <yahg.concept@gmail.com>
2022-06-05 08:13:08 -0700

    all commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,21 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
 int i;
 int y;
+int z;
 for (i = 0; i < 10; i++)
 {
 for (y = 0; y < 10; y++)
 {
 if (i == 9 && y == 9)
 {
-printf("%d%d", i, y);
+printf("%d%d %d%d", i, i, i, y);
 }
 else
-printf("%d%d, ", i, y); 
+printf("%d%d %d%d, ", i, i, i, y);
 }
 }
 putchar('\n');
