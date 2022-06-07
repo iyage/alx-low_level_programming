e53c6c4b7f9d894ebc5592e73cde4ab810b865d1
iyage <yahg.concept@gmail.com>
2022-06-07 05:33:21 -0700

    all commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("%s\n", "_putchar");
+return (0);
+}
