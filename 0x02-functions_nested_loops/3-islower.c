c28124d29afff043224a2eecf3528c865b57e746
iyage <yahg.concept@gmail.com>
2022-06-07 14:22:10 -0700

    all commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _islower - function that check if a character is lower case or not
+ */
+
+int _islower(int c)
+{
+{
+if( c >= 97 && c <= 122 )
+return 1;
+else
+return 0;
+}
+}
