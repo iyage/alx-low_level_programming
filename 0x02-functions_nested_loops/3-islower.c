bfd9b54daf90f66e25fe64d5dc59d0126fcd7198
iyage <yahg.concept@gmail.com>
2022-06-07 14:36:11 -0700

    all commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,13 @@
 
 /**
  * _islower - function that check if a character is lower case or not
- * Return - on success  1
- * Return - failure  0
+ * Return - return  1 or 0
  */
 
 int _islower(int c)
 {
 {
-if(c >= 97 && c <= 122)
+if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
