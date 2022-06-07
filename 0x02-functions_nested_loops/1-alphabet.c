eb5f0fffae6e86c63562b7208296bf098f67d5d5
iyage <yahg.concept@gmail.com>
2022-06-07 12:57:32 -0700

    all commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 /* more headers goes there */
 
 /**
- * _putchar - Entry point
- *
  * print_alphabet - function that print lower case alphabet
  * 
  * Return: Always 0 (Success)
@@ -13,7 +11,7 @@ void print_alphabet(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-	_putchar(c);
+_putchar(c);
 }
 _putchar('\n');
 }
