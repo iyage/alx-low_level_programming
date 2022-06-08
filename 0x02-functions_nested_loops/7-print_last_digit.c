fe00d160d077c18a4fd4fc859cd116395f8beb2d
iyage <yahg.concept@gmail.com>
2022-06-07 21:54:52 -0700

    all commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit(int) - function that print last digit of number
+ * @n: last digit of n is printed
+ * Return:  last digit of n
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+
+}
