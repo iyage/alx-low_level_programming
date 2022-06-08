c85f858408db81c070266cd286503841ef80b801
iyage <yahg.concept@gmail.com>
2022-06-07 21:27:39 -0700

    all commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,8 @@
  */
 int print_sign(int n)
 {
-if (n < 0){
+if (n < 0)
+{
 _putchar('-');
 return (-1);
 }
@@ -15,10 +16,9 @@ else if (n > 0)
 _putchar('+');
 return (1);
 }
-else 
+else
 {
 _putchar('0');
 return (0);
 }
-
 }
