8ff3b6234584bbe64e9fd2ce8dbf0766fb747798
iyage <yahg.concept@gmail.com>
2022-06-08 08:36:39 -0700

    all commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *add - function that add two numbers together
+ *@a:first number
+ *@b:second number
+ * Return: return their result
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
