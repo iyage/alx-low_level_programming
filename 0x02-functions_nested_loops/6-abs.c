d1139058ac11746dfb79ac63c43bfc3960bb38fb
iyage <yahg.concept@gmail.com>
2022-06-07 21:47:41 -0700

    all commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _abs(int) - function that print abs of an int
+ * @n: absolute of n is printed
+ * Return:  return abs of n
+ */
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else
+{
+n = -1 * n;
+return (n);
+}
+
+}
