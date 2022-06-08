38fca61d98d8452c6edb54bfc9e2765bf22ef3c4
iyage <yahg.concept@gmail.com>
2022-06-07 21:25:47 -0700

    all commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,24 @@
 #include "main.h"
 /**
- * _islower - function that check if a character is lower case or not
+ * print_sign - function that check if number is negative or poitive
  * @n: integer to be checked for sign
  * Return:  return -1 or 1 or 0
  */
 int print_sign(int n)
 {
-if (n < 0)
-{
-    _putchar('-');
-    return (-1);
+if (n < 0){
+_putchar('-');
+return (-1);
 }
 else if (n > 0)
 {
-    _putchar('+');
-    return (1);
+_putchar('+');
+return (1);
 }
-else
+else 
 {
-    _putchar('0');
-    return (0);
+_putchar('0');
+return (0);
 }
 
 }
