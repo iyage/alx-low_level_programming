2b012fc22b7f07bec43ec07addc42259af0474a4
iyage <yahg.concept@gmail.com>
2022-06-07 21:08:51 -0700

    all commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _islower - function that check if a character is lower case or not
+ * @n: integer to be checked for sign
+ * Return:  return -1 or 1 or 0
+ */
+int print_sign(int n)
+{
+if (n < 0){
+    _putchar('-');
+    _putchar(',');
+    return (-1);
+}
+else if (n > 0)
+{
+    _putchar('+');
+    _putchar(',');
+    return (1);
+}
+else 
+    _putchar(0);
+    _putchar(',');
+    return (0);
+
+}
