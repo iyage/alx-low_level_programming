0ab24b3e2f8578804aeb50744fcf370332ed9431
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 06:35:50 +0100

    re-adjust 1-main.c file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* main - remove the cause of infinite loop
-*@'\\o/\n"': for new line and back slash
+* main - causes an infinite loop
 * Return: 0
 */
 
@@ -15,10 +13,10 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
-* while (i < 10)
-*{
-*putchar(i);
-*}
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
  */
 printf("Infinite loop avoided! \\o/\n");
 
