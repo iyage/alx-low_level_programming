e0189ee3633acdfd3750f2c4b4961171dbde6d5c
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 13:25:00 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - function to print line base on input
+ * @n:input
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+}
