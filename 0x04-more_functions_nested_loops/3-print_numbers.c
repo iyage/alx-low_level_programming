16ba84dae078b12c43809c0848300dc562b2f67c
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 12:06:07 +0100

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - function to print 0-9
+ */
+void print_numbers(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+_putchar(i);
+_putchar('\n');
+}
