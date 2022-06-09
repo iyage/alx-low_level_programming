8fab0a1aeb7c7a474c08f234c496ca03a6dd1c16
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 04:57:57 +0100

    task-0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function to test the sign of an integer
 * Return: 0
 */
 
 int main(void)
 {
-int i;
-
-i = 0;
-positive_or_negative(i);
-
+int n = 0;
+positive_or_negative(n);
 return (0);
 }
