7c62e8c9ba911dceecf0ae7a4776428a4d1fe6ff
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 12:14:14 +0100

    adjust main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 int _putchar(char c);
 int _isupper(int);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isdigit(int);
+int mul(int, int);
  void print_numbers(void);
  void print_most_numbers(void);
  void more_numbers(void);
