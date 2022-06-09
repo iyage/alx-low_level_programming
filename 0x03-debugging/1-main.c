70e5f15fe4b128f5988d6b5fffcbecd7f7079171
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 05:14:10 +0100

    remove infinite loop causing line

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - remove the cause of infinite loop
+*@'\\o/\n"': for new line and back slash
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/**
+* while (i < 10)
+*{
+*putchar(i);
+*}
+ */
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
