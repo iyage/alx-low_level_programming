d2fb0ca3f31c37a9d62c59e075993c928b204b5c
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 13:02:38 +0100

    print 0-14 10X

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - function to print 0-14 10 times
+ */
+void more_numbers(void)
+{
+int x;
+int y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+if (y <= 9)
+_putchar(y + '0');
+else
+{
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
