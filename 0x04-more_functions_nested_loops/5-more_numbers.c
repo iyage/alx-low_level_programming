2c9b6e35a5ea991b44ec214ba870cbb9ab26c0d1
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 13:16:09 +0100

    print 0-14 10X

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,11 @@ for (x = 0; x < 10; x++)
 {
 for (y = 0; y < 15; y++)
 {
-if (y <= 9)
-_putchar(y + '0');
-else
+if (y > 9)
 {
 _putchar((y / 10) + '0');
-_putchar((y % 10) + '0');
 }
+_putchar((y % 10) + '0');
 }
 _putchar('\n');
 }
