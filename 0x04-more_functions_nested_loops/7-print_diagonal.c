78e6f9425f55ea11810263431e79f10fc1163640
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 13:49:05 +0100

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagonal - function to print \ base on input
+ * @n:input
+ */
+void print_diagonal(int n)
+{
+int i = 1;
+int y;
+while (n > 0)
+{
+for (y = 0; y < i; y++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+n--;
+}
+}
