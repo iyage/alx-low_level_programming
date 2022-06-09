aad47af1dac1640d65bc4bde452914fc7487c925
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 12:43:55 +0100

    print 0-9 without 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - function to print 0-9 omit 2&4
+ */
+void print_most_numbers(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i == '2' || i == '4')
+continue;
+_putchar(i);
+}
+
+_putchar('\n');
+}
