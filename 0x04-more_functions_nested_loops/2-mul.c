eeeac8477cce10fc9067cded8d2bd70ed49eca60
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-09 11:51:59 +0100

    multiply two nos

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - function to check letter case
+ * @a: first operand
+ * @b: second operand
+ * Return: return the result of mul of the 2 nos
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
