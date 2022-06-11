9b54a06396cfa1992f3e21fe9ffbc74941e46c8c
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-11 19:03:38 +0100

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Prints a number using recursion
+* @n: The number to print
+*/
+void print_number(int n)
+{
+unsigned int numb = n;
+if (n < 0)
+{
+_putchar('-');
+		numb = -numb;
+}
+if (numb > 9)
+{
+_print_number(numb / 10);
+}
+_putchar(numb % 10 + '0');
+}
