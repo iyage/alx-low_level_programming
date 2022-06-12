84051849e13eb671a3ab3079b66d90f299498348
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-12 12:13:21 +0100

    print number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_times_table  - function that print times table up to n
+ * @n: times table
+ */
+void print_times_table(int n)
+{
+int num, mul, result;
+if (n < 0 || n > 15)
+_putchar('\n');
+else
+{
+for (num = 0; num <= n; num++)
+{
+for (mul = 0; mul <= n; mul++)
+{
+result = num * mul;
+if (result > 9)
+{
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+if (mul != 9)
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(num * mul + '0');
+if (mul != 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
+}
