f3ebe5be836d40e042bfc5539faa3d461eda0fca
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-12 12:04:12 +0100

    print number

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,17 @@ int main(void)
 {
 unsigned long a = 0;
 unsigned long  b = 1;
-unsigned long c;
+unsigned long c = 0;
 unsigned long sum = 0;
-int i;
-for (i = 0; i < 98; i++)
+while (c <= 4000000)
 {
 c = a + b;
 a = b;
 b = c;
-if (c == 4000000)
-break;
-if (c % 2 == 0)
+if ((c % 2) == 0)
 sum += c;
 }
 printf("%lu\n", sum);
 return (0);
 }
+
