c3f1d9565dce57ce302c91e90fa8eacfd74a8c05
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-12 11:23:12 +0100

    print number

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: return 0
+ */
+int main(void)
+{
+unsigned long int a = 0;
+unsigned long int b = 1;
+unsigned long int c;
+int i;
+for (i = 0; i < 50; i++)
+{
+c = a + b;
+a = b;
+b = c;
+printf("%ld, ", c);
+}
+printf("\n");
+return (0);
+}
