7032a415eaadf1052149e4fc3b1e7d55ae3b80ff
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-13 20:58:38 +0100

    2-strlen.c task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+int count = 0;
+while(*(s+i) != '\0')
+{
+
+count++;
+i++;
+}
+return (count);
+}
