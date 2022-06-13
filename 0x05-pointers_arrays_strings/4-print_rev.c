bbd5901efc0b770370ce7d6425a9c076b060d2c9
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-13 21:26:50 +0100

    4-print_rev.c task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *print_rev- returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
+ */
+
+
+void print_rev(char *s)
+{
+int i = 0;
+
+while (*(s + i) != '\0')
+{
+i++;
+}
+
+while (i > 0)
+{
+putchar(*(s + i));
+i--;
+}
+printf("\n");
+}
+
