dcae1aa859b54f7ba6b77efd878b93df447680be
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-14 17:12:16 +0100

    advance task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,15 +11,17 @@
 
 int main(void)
 {
-srand(time(0));
 int lower = 32;
 int upper = 126;
-    int num;
-for(int i = 0; i<32; i++)
-    {
+int num;
+int i = 0;
+srand(time(0));
+
+for (i = 0; i<32; i++)
+{
 num = (rand()%(upper - lower + 1) + lower );
 printf("%c", num);
-    }
+}
 putchar(10);
 return 0;
 }
