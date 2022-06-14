c563d41e6a7b29c262b61c5838990c97b50fe78d
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-14 04:48:02 +0100

    7-puts_half.c task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,12 +2,17 @@
 #include <string.h>
 /**
  * puts_half- print half string
+ *  * @str: string
  */
 void puts_half(char *str)
 {
 int i;
 int l = strlen(str);
-int half_length = l / 2;
+int half_length;
+if (l % 2 = 0)
+ half_length = l / 2;
+ else
+ half_length = (l - 1) / 2;
 i = half_length;
 while (i < l)
 {
