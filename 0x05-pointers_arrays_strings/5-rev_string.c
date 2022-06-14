37bf538e32c6d4b44d44a63a51e3eda1aa40fba8
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-14 04:10:51 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <string.h>
+/**
+ *rev_string -  function to reverse string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+char *start_add;
+char *end_add;
+char ch;
+l = strlen(s);
+start_add = s;
+end_add = s;
+for (i = 0; i < l - 1; i++)
+end_add++;
+for (i = 0; i < l / 2; i++) {
+ch = *end_add;
+*end_add = *start_add;
+*start_add = ch;
+ 
+start_add++;
+end_add--;
+    }
+}
