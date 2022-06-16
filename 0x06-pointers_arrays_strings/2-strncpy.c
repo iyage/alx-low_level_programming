639798e097a3a5ed348bc4897f55f0e182efff1b
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-16 13:57:05 +0100

    2-strncpy.c task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncpy - concatenation function
+ * @dest: destination string
+ * @src: string to copy
+ * @n: size
+ * Return: char* pointer is return;
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *src++;
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
