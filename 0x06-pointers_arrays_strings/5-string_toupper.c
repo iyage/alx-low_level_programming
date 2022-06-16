74865c527baf57086f932eb76d8e838f1819838e
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-16 16:01:31 +0100

    5-string_toupper.c task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,11 +7,12 @@
  */
 char *string_toupper(char *str)
 {
-while (*str != '\0')
+int i = 0;
+while (*(str + i) != '\0')
 {
-if (*str >= 'a' && *str <= 'z')
-*str = *str - 32;
-*str++;
+if (*(str + i) >= 'a' && *(str + i) <= 'z')
+*(str + i) = *(str + i) - 32;
+i++;
 }
 return (str);
 }
