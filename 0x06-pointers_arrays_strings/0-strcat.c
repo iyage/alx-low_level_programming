21459ed2df6266ae35355a593b04e30df5d2fa1f
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-16 11:49:02 +0100

    0-strcat.c task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strcat - concatenation function
+ * @dest: string to concatenate to
+ * @src: string to concatenate
+ * Return: char* pointer is return;
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+char *startStr;
+char *endStr;
+startStr = dest;
+endStr = src;
+int startStrlen;
+startStrlen = strlen(dest);
+while (*endStr != '\0')
+{
+*(startStr + startStrlen + i) = *endStr++;
+i++;
+}
+*(startStr + startStrlen + i) = '\0';
+return (startStr);
+}
