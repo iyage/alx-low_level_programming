64032442de1dbe1d1583721b16b8bd8df05734d9
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-21 19:49:53 +0100

    8-print_diagsums.c  task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - 
+ * print_diagsums - function to diagonal sum
  * @a: string to be searched
  * @size: string to be used
- *
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-    int j = 0;
-    int sum1 = 0;
-    int  sum2 = 0;
+int i = 0;
+int j = 0;
+int sum1 = 0;
+int  sum2 = 0;
 
-	for (; i < size * size; i += size)
-	{
-		sum1 += a[i + j];
-		sum2 += a[i + (size - 1) - j];
-		j++;
-	}
-	printf("%d, %d\n", sum1, sum2);
+for (; i < size * size; i += size)
+{
+sum1 += a[i + j];
+sum2 += a[i + (size - 1) - j];
+j++;
+}
+printf("%d, %d\n", sum1, sum2);
 }
