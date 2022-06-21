74aba9dbb52f655296a1e4d8ba41af8ff4e30bc7
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-21 04:28:59 +0100

    1-memcpy.c task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,18 +5,15 @@
  * @dest:  destination memory
  * @src: source memory
  * @n: number of bytes to be copied
- *
  * Return: pointer to the copied memory
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i=0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	return (dest);
+unsigned int i;
+for (i=0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
