309b0714697f950e1bef2dc223c29f90f6a5a77b
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-22 22:25:10 +0100

    4-pow_recursion.c task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _pow_recursion - function to print power of number
+ *@x: base
+ *@x: index
+ *Return: intger value
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(int x, int y - 1));
+}
