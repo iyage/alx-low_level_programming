aeb6c00669426397ef0126f8f9a769acb44124b5
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-22 20:42:15 +0100

    1-print_rev_recursion.c task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+/**
+ *  _print_rev_recursion - function to print char in reverse order
+ *@s: char string to print
+ */
+ void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
