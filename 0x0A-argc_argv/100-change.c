dc60d075366e4ca843482001fcb3363ab5b4d43b
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-24 15:47:14 +0100

    100-change.c task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *cents - function to minimal change
+ * @amount: argument value
+ */
+void cents(int amount)
+{
+int change = 0;
+while (amount > 0)
+{
+if (amount % 25 == 0)
+{
+change++;
+amount -= 25;
+}
+else if (amount % 10 == 0)
+{
+change++;
+amount -= 10;
+}
+else if (amount % 5 == 0)
+{
+change++;
+amount -= 5;
+}
+else if (amount % 2 == 0)
+{
+change++;
+amount -= 2;
+}
+else if (amount % 1 == 0)
+{
+change++;
+amount -= 1;
+}
+}
+printf("%d\n", change);
+}
+
+/**
  *main - main entry
  * @argc: length of command line argument
  * @argv: array of command line argument
@@ -8,14 +46,13 @@
  */
 int main(int argc, char *argv[])
 {
-int change = 0;
 int amount = atoi(argv[argc - 1]);
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-else 
+else
 {
 if (amount < 0)
 {
@@ -23,42 +60,8 @@ printf("0\n");
 }
 else
 {
-while (amount > 0)
-{
-if (amount % 25 == 0)
-{
-change++;
-amount -= 25;
-continue;
-}
-if (amount % 10 == 0)
-{
-change++;
-amount -= 10;
-continue;
-}
-if (amount % 5 == 0)
-{
-change++;
-amount -= 5;
-continue;
-}
-if (amount % 2 == 0)
-{
-change++;
-amount -= 2;
-continue;
-}
-if (amount % 1 == 0)
-{
-change++;
-amount -= 1;
-continue;
-}
-}
-}
-}
-
-printf("%d\n", change);
+cents(amount);
 return (0);
+}
+}
 }
