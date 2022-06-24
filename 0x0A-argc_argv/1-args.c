07bfd6198868c9e7cebc426c0c4a87ac236175f4
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-24 14:00:00 +0100

    1-args.c task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main entry
+ * @argc: length of command line argument
+ * @argv: array of command line argument
+ * Return: always return 0
+ */
+int main(int argc, char* argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("d\n", i);
+return (0);
+}
