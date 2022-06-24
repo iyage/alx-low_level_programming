736d795a6a44cabfbe73a338771e76a29955a6d5
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-24 13:51:12 +0100

    0-whatsmyname.c task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main entry
+ * @argc: length of command line argument
+ * @argv: array of command line argument
+ * Return: always return 0
+ */
+
+
+int main(int argc, char* argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
