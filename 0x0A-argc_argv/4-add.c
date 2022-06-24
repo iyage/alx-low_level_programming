e83ccfac3afe403eb5731f0ec94fc6b9e8dfd6ba
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-24 15:01:58 +0100

    4-add.c task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main entry
+ * @argc: length of command line argument
+ * @argv: array of command line argument
+ * Return: always return 0
+ */
+int main(int argc, char *argv[])
+{
+int i, j, sum = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
